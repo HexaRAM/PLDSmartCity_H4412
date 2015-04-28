c71a7f6e3c15c2274fc52f9e2d292d94c94eca8a
Quentin Lemaire <quentyn.lemaire@gmail.com>
2015-04-28 09:50:52 +0200

    start toolbar

--- android/app/src/main/java/hexaram/challengelyon/MainActivity.java
+++ android/app/src/main/java/hexaram/challengelyon/MainActivity.java
@@ -2,16 +2,23 @@ package hexaram.challengelyon;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 
 
@@ -31,6 +38,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(this, "Hey l'HexaRAM+, pas encore de paramètres :(", Toast.LENGTH_SHORT).show();
             return true;
         }
 
