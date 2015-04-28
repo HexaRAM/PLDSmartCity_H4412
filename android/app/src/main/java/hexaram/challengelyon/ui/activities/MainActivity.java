b3a96ccee11ef5f4cf3e51ee18694a7892c97019
William Vitali <ebill@hotmail.fr>
2015-04-28 11:40:25 +0200

    Profile View

--- android/app/src/main/java/hexaram/challengelyon/ui/activities/MainActivity.java
+++ android/app/src/main/java/hexaram/challengelyon/ui/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package hexaram.challengelyon;
+package hexaram.challengelyon.ui.activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import hexaram.challengelyon.R;
 
 
 public class MainActivity extends ActionBarActivity {
