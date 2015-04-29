9bfe721463a5e430f2fd45a7edf39d2863489a33
William Vitali <ebill@hotmail.fr>
2015-04-29 11:42:53 +0200

    Challenge List

---
+++ android/app/src/main/java/hexaram/challengelyon/ui/adapter/ChallengeAdapter.java
@@ -0,0 +1,47 @@
+package hexaram.challengelyon.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.util.List;
+
+import hexaram.challengelyon.R;
+import hexaram.challengelyon.models.Challenge;
+
+/**
+ * Created by William on 29/04/2015.
+ */
+public class ChallengeAdapter<T> extends ArrayAdapter{
+
+    public ChallengeAdapter(Context context, int resource, List<T> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+
+        // Get the data item for this position
+        Challenge challenge = (Challenge) getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.challenge_list_item, parent, false);
+        }
+        // Lookup view for data population
+        TextView textItemTitle = (TextView) convertView.findViewById(R.id.challenge_list_item_title);
+        TextView textItemSummary = (TextView) convertView.findViewById(R.id.challenge_list_item_summary);
+        TextView textItemReward = (TextView) convertView.findViewById(R.id.challenge_list_item_reward);
+
+        textItemTitle.setText(challenge.getTitle());
+        textItemSummary.setText(challenge.getSummary());
+        textItemReward.setText(""+challenge.getReward());
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
---
+++ android/app/src/main/java/hexaram/challengelyon/ui/fragments/HotFragment.java
@@ -0,0 +1,100 @@
+package hexaram.challengelyon.ui.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import hexaram.challengelyon.R;
+import hexaram.challengelyon.models.Challenge;
+import hexaram.challengelyon.ui.activities.MainActivity;
+import hexaram.challengelyon.ui.adapter.ChallengeAdapter;
+
+
+public class HotFragment extends Fragment {
+
+    protected ArrayList<Challenge> challengeList;
+    protected ChallengeAdapter<Challenge> adapter;
+    protected ListView challengeListView;
+
+    public HotFragment() {
+    }
+
+    public static HotFragment newInstance(ArrayList<Challenge> list) {
+        HotFragment challengeFragment = new HotFragment();
+        Bundle args = new Bundle();
+        args.putSerializable("Hot", list);
+        challengeFragment.setArguments(args);
+        return challengeFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+//        if (getActivity() instanceof MainActivity) {
+//            ((MainActivity) getActivity()).addOnListChangedListener(this);
+//        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.challenge_list, container, false);
+
+        challengeList = (ArrayList<Challenge>) getArguments().getSerializable("Hot");
+        getArguments().remove("Hot");
+
+        //Initialiser l'adapter
+        adapter = new ChallengeAdapter<>(getActivity(), R.layout.challenge_list_item, challengeList);
+        challengeListView = (ListView) rootView.findViewById(R.id.challenge_list);
+        //L'appliquer sur la listView
+        challengeListView.setAdapter(adapter);
+
+//        challengeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+////            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+////                Challenge c = (Challenge) parent.getItemAtPosition(position);
+////                Intent intent = new Intent(getActivity(), DetailsActivity.class);
+////                intent.putExtra("challenge", c);
+//////                String transitionName = getString(R.string.challenge_details_transition);
+//////                ActivityOptionsCompat options =
+//////                        ActivityOptionsCompat.makeSceneTransitionAnimation(getActivity(),
+//////                                view,   // The view which starts the transition
+//////                                transitionName    // The transitionName of the view we’re transitioning to
+//////                        );
+//////                ActivityCompat.startActivity(getActivity(), intent, options.toBundle());
+////            }
+//        });
+        return rootView;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+//        if (getActivity() instanceof MainActivity) {
+//            ((MainActivity) getActivity()).removeOnListChangedListener(this);
+//        }
+    }
+
+    public void onListChanged(List<Challenge> newList) {
+        if (newList == null) {
+            adapter = new ChallengeAdapter<>(getActivity(), R.layout.challenge_list_item, challengeList);
+        } else {
+            adapter = new ChallengeAdapter<>(getActivity(), R.layout.challenge_list_item, newList);
+        }
+        challengeListView.setAdapter(adapter);
+    }
+
+}
