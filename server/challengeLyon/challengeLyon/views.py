2fd9c927ce6ffcc21cd9fa109276af177b9a7bff
Quentin Lemaire <quentyn.lemaire@gmail.com>
2015-05-04 16:42:08 +0200

    refactor stuff / add challengePlayed list & detailled services / improving picture upload

--- server/challengeLyon/challengeLyon/views.py
+++ server/challengeLyon/challengeLyon/views.py
@@ -1,5 +1,7 @@
 #-*- coding: utf-8 -*-
 
+from django.shortcuts import get_object_or_404
+
 from rest_framework import viewsets
 from rest_framework import generics
 from rest_framework import status
@@ -7,8 +9,13 @@ from rest_framework.response import Response
 from rest_framework.decorators import detail_route
 from django.conf import settings
 from challengeLyon.models import *
+from challengeLyon.serializers import *
 
 from django.db.models import Count
+
+class ValidationItemViewSet(viewsets.ModelViewSet):
+    queryset = Validationitem.objects.all()
+    serializer_class = ValidationItemSerializer
 
 class LocationViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.all()
@@ -18,25 +25,45 @@ class ChallengeViewSet(viewsets.ModelViewSet):
     queryset = Challenge.objects.all()
     serializer_class = ChallengeSerializer
 
-# TODO : remove this viewset (la validation se fait automatiquement quand un utilisateur vote sur un challenge)
-# Sert d'exemple pour créer des vues custom
 class ChallengePlayedViewSet(viewsets.ViewSet):
-    """
-        Validation d'un challenge joué
-    """
-    @detail_route(methods=['get'])
-    def validate(self, request, pk=None):
-        print pk
-        if pk is not None:
-            try:
-                challengeplayed = Challengeplayed.objects.get(id=pk)
-                challengeplayed.validate()
-                challengeplayed.save()
-                return Response({'status': 'challenge validated'})
-            except:
-                return Response("Wrong Challenge Played", status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response("Unknown id", status=status.HTTP_400_BAD_REQUEST)
+    def list(self, request):
+        query = Challengeplayed.objects.filter(user=request.user)
+        serializer = ChallengePlayedListSerializer(query, many=True, read_only=True, context={'request': request})
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        query = Challengeplayed.objects.all()
+        challengeplayed = get_object_or_404(query, pk=pk)
+        serializer = ChallengePlayedSerializer(challengeplayed, read_only=True, context={'request': request})
+        return Response(serializer.data)
+
+    # @detail_route(methods=['get'])
+    # def validate(self, request, pk=None):
+    #     if pk is not None:
+    #         try:
+    #             challengeplayed = Challengeplayed.objects.get(id=pk)
+    #             challengeplayed.validate()
+    #             challengeplayed.save()
+    #             return Response({'status': 'challenge validated'})
+    #         except:
+    #             return Response("Wrong Challenge Played", status=status.HTTP_400_BAD_REQUEST)
+    #     else:
+    #         return Response("Unknown id", status=status.HTTP_400_BAD_REQUEST)
+
+    @detail_route(methods=['GET', 'POST'])
+    def pictures(self, request, pk):
+        if request.method == "GET":
+            pictures = Picture.objects.filter(validationitem__challengeplayed__id=pk)
+            serializer = PictureSerializer(pictures, context={'request': request}, many=True, read_only=True)
+            return Response(serializer.data)
+
+        if request.method == "POST":
+            serializer = PictureSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'status': 'picture uploaded'})
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class HotChallengeViewSet(generics.ListAPIView, viewsets.GenericViewSet):
